1bfbe078bfb80dbfdd6f30539dd2e209541eab06
Ivan Polyakov <arschvin@gmail.com>
2021-10-04 00:38:24 +0300

    Added docker and docker ignore files. Modified build.gradle file in order to add docker build plugin

--- webapp-spring-boot/src/main/java/prived/medved/controllers/EntryPoint.java
+++ webapp-spring-boot/src/main/java/prived/medved/controllers/EntryPoint.java
@@ -1,72 +1,69 @@
 package prived.medved.controllers;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import prived.medved.response.*;
+import prived.medved.response.enums.EntityType;
+import prived.medved.response.enums.IPsCount;
+
 import java.net.*;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.*;
 import java.util.stream.Collectors;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
-import prived.medved.response.*;
-import prived.medved.response.enums.EntityType;
-import prived.medved.response.enums.IPsCount;
 
 @RestController
 public class EntryPoint {
 
-  @GetMapping("/date")
-  public SimpleEntity getNowDate() {
+    @GetMapping("/date")
+    public SimpleEntity getNowDate(){
 
-    return new SimpleEntity(
-        EntityType.DATE_NOW, LocalDate.now().format(DateTimeFormatter.ofPattern("yyyy MM dd")));
-  }
-
-  @GetMapping("/time")
-  public SimpleEntity getNowTime() {
-
-    return new SimpleEntity(
-        EntityType.TIME_NOW, LocalDateTime.now().format(DateTimeFormatter.ofPattern("hh:mm:ss")));
-  }
-
-  @GetMapping("/ip")
-  public IfaceEntity getIP() throws SocketException {
-
-    Enumeration<NetworkInterface> ifaces = NetworkInterface.getNetworkInterfaces();
-
-    List<IFaceIPPair> ipList =
-        Collections.list(ifaces).stream()
-            .map(
-                iface -> {
-                  String name = iface.getDisplayName();
-                  LinkedList<IFaceIPPair> addrList = new LinkedList<IFaceIPPair>();
-
-                  for (InetAddress ifAddr : Collections.list(iface.getInetAddresses())) {
-
-                    if (ifAddr instanceof Inet4Address) {
-                      addrList.add(new IFaceIPPair(name, ifAddr.getHostAddress()));
-                    }
-                  }
-                  return addrList;
-                })
-            .flatMap(ifList -> ifList.stream())
-            .collect(Collectors.toList());
-
-    IPsCount count;
-
-    if (ipList.size() % 2 == 0) {
-      count = IPsCount.EVEN;
-    } else {
-      count = IPsCount.ODD;
+        return new SimpleEntity(EntityType.DATE_NOW, LocalDate.now().format(DateTimeFormatter.ofPattern("yyyy MM dd")));
     }
-    return new IfaceEntity(EntityType.IP_V4, count, ipList);
-  }
 
-  @GetMapping("/cpu")
-  public SimpleEntity getCPUs() {
+    @GetMapping("/time")
+    public SimpleEntity getNowTime(){
 
-    int cpuCount = Runtime.getRuntime().availableProcessors();
+        return new SimpleEntity(EntityType.TIME_NOW, LocalDateTime.now().format(DateTimeFormatter.ofPattern("hh:mm:ss")));
+    }
 
-    return new SimpleEntity(EntityType.CPU_AMOUNT, String.valueOf(cpuCount));
-  }
+    @GetMapping("/ip")
+    public IfaceEntity getIP() throws SocketException {
+
+        Enumeration<NetworkInterface> ifaces = NetworkInterface.getNetworkInterfaces();
+
+        List<IFaceIPPair> ipList = Collections.list(ifaces).stream().map(iface -> {
+
+            String name = iface.getDisplayName();
+            LinkedList<IFaceIPPair> addrList = new LinkedList<IFaceIPPair>();
+
+            for (InetAddress ifAddr : Collections.list(iface.getInetAddresses())){
+
+                if (ifAddr instanceof Inet4Address){
+                    addrList.add(new IFaceIPPair(name, ifAddr.getHostAddress()));
+                }
+            }
+            return addrList;
+
+        }).flatMap(ifList -> ifList.stream()).collect(Collectors.toList());
+
+        IPsCount count;
+
+        if (ipList.size() % 2 == 0) {
+            count = IPsCount.EVEN;
+        } else {
+            count = IPsCount.ODD;
+        }
+        return new IfaceEntity(EntityType.IP_V4, count, ipList);
+    }
+
+    @GetMapping("/cpu")
+    public SimpleEntity getCPUs(){
+
+        int cpuCount = Runtime.getRuntime().availableProcessors();
+
+        return new SimpleEntity(EntityType.CPU_AMOUNT, String.valueOf(cpuCount));
+    }
 }
