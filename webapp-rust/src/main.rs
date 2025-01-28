ed3bc10af216acdf77e90abdfff5a6286c542701
Ivan Polyakov <arschvin@gmail.com>
2025-01-28 16:40:38 +0100

    First steps

--- webapp-rust/src/main.rs
+++ webapp-rust/src/main.rs
@@ -1,3 +1,32 @@
+use std::env;
+
+use seahorse::{App, Flag, FlagType, Context};
+// use std::env;
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+
+    let app = App::new(env!("CARGO_PKG_NAME"))
+    .description(env!("CARGO_PKG_DESCRIPTION"))
+    .author(env!("CARGO_PKG_AUTHORS"))
+    .flag(
+        Flag::new("address", FlagType::String)
+        .description("Bind address to (default: 0.0.0.0)").alias("a")
+    )
+    .flag(
+        Flag::new("port", FlagType::String)
+        .description("Bind port to (required)").alias("p")
+    ).action(default_action);
+
+    app.run(args);
+}
+
+fn default_action(c: &Context){
+    let address = match c.string_flag("address") {
+        Ok(addr) => addr,
+        Err(_) => "0.0.0.0".to_string(),
+    };
+
+    let port = c.string_flag("port").expect("'port' flag is required");
+    
+    println!("Gathered args: {}:{}",address,port);
 }
