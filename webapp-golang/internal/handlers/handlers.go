76465e527c10bcc2b7d244701ac6f44f2bf0c0e9
Ivan Polyakov <arschvin@gmail.com>
2025-08-06 23:07:44 +0200

    Webapp golang (#4)
    * Added some thoughts
    
    * Tmp commit
    
    * Implemented /cpu & /ip url paths
    
    * Implemented /date & /time url paths
    
    * Added couple of README files, filled them and activated golang linter
    
    * Removed readme.md files with different cases
    
    * Added uppercases named readme files
    
    ---------
    
    Co-authored-by: Ivan Poliakov <ivan_poliakov@epam.com>

---
+++ webapp-golang/internal/handlers/handlers.go
@@ -0,0 +1,84 @@
+package handlers
+
+import (
+	"net/http"
+	"net/netip"
+	"strings"
+	"time"
+
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/gommon/log"
+	gopsutilCpu "github.com/shirou/gopsutil/v4/cpu"
+	gopsutilNet "github.com/shirou/gopsutil/v4/net"
+)
+
+func CpuAmount(c echo.Context) error {
+	nproc, err := gopsutilCpu.Counts(true)
+	if err != nil {
+		log.Errorf("Could not get CPU amount: %s", err)
+		return err
+	}
+
+	return c.JSON(http.StatusOK, &CpuResponse{
+		EntityType: entityTypeOutput(cpuAmount),
+		Payload:    nproc,
+	})
+}
+
+func AddrIpV4Amount(c echo.Context) error {
+	interfaces, err := gopsutilNet.Interfaces()
+	if err != nil {
+		log.Errorf("Could not get Net Interfaces info: %s", err)
+	}
+
+	response := &AddrIpV4Response{
+		EntityType: entityTypeOutput(addrIpV4Amount),
+	}
+	iFsInfo := make([]*IfInfo, 0, 5)
+
+	for _, ifConf := range interfaces {
+		for _, ifIpAddr := range ifConf.Addrs {
+
+			// Due to gopsutil returns IP address with MASK in one string which will not be parsed, we need only first of both
+			ipAddr := strings.Split(ifIpAddr.Addr, "/")[0]
+			ipParsed, err := netip.ParseAddr(ipAddr)
+			if err != nil {
+				log.Errorf("Could not parse IP address: %s: %s", ipAddr, err)
+				return err
+			}
+
+			if ipParsed.Is4() {
+				iFsInfo = append(iFsInfo, &IfInfo{
+					Name:      ifConf.Name,
+					IpAddress: ipParsed.String(),
+				})
+			}
+		}
+	}
+
+	response.Payload = iFsInfo
+
+	if len(iFsInfo)%2 != 0 {
+		response.Count = parityOutput(odd)
+	} else {
+		response.Count = parityOutput(even)
+	}
+
+	return c.JSON(http.StatusOK, response)
+}
+
+func CurrentDateTime(c echo.Context, dateOnly bool) error {
+	response := &DateTimeNow{}
+	now := time.Now()
+
+	switch dateOnly {
+	case true:
+		response.EntityType = entityTypeOutput(dateNow)
+		response.Payload = now.Format(time.DateOnly)
+	case false:
+		response.EntityType = entityTypeOutput(timeNow)
+		response.Payload = now.Format(time.TimeOnly)
+	}
+
+	return c.JSON(http.StatusOK, response)
+}
---
+++ webapp-golang/magefile.go
@@ -0,0 +1,64 @@
+//go:build mage
+// +build mage
+
+package main
+
+import (
+	"context"
+	"fmt"
+	"os"
+	"path"
+
+	"github.com/magefile/mage/mg" // mg contains helpful utility functions, like Deps
+	"github.com/magefile/mage/sh"
+)
+
+const (
+	appPackagePath = "./cmd/test-webapp"
+)
+
+// Default target to run when none is specified
+// If not set, running mage will list available targets
+var Default = BuildMainApp
+
+// A build target of the Main App (tf-plan-reporter)
+func BuildMainApp(ctx context.Context) error {
+
+	fmt.Printf("Building of '%s' ...\n", path.Base(appPackagePath))
+
+	envs := make(map[string]string)
+	if os.Getenv("CGO_ENABLED") != "" {
+		envs["CGO_ENABLED"] = os.Getenv("CGO_ENABLED")
+		fmt.Printf("CGO_ENABLED environment variable was set up to: %s\n", os.Getenv("CGO_ENABLED"))
+	}
+
+	return build(envs, path.Base(appPackagePath), appPackagePath)
+}
+
+// Executes golang build command
+func build(envs map[string]string, binName, packPath string) error {
+	return sh.RunWith(envs, "go", "build", "-o", binName, packPath)
+}
+
+// Executes golang test command
+func runTest(envs map[string]string) error {
+	return sh.RunWith(envs, "go", "test", "./...")
+}
+
+// A custom install step to target folder
+func Install(targetFolder string) error {
+	mg.Deps(BuildMainApp)
+
+	if targetFolder == "" {
+		fmt.Errorf("Target folder must not be an empty string")
+	}
+
+	fmt.Printf("Installing to %s...\n", targetFolder)
+	return os.Rename(path.Base(appPackagePath), fmt.Sprintf("%s/%s", targetFolder, path.Base(appPackagePath)))
+}
+
+// Clean up after yourself
+func Clean() {
+	fmt.Printf("Cleaning of %s...\n", path.Base(appPackagePath))
+	os.RemoveAll(path.Base(appPackagePath))
+}
