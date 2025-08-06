5b728457c9865f781f0f8c6e37204f727a1fab9d
Ivan Polyakov <arschvin@gmail.com>
2025-08-06 15:09:39 +0200

    CPU_AMOUNT is ready

--- webapp-rust/src/handlers.rs
+++ webapp-rust/src/handlers.rs
@@ -1,15 +1,37 @@
-use poem::handler;
-// use serde::Serialize;
+use poem::{handler, web::Json};
+use serde::Serialize;
+use sysinfo::{CpuRefreshKind, System};
 
-// #[derive(Serialize, Debug)]
-// struct CpuAmount {
-//     r#type: String,
-//     payload: u8,
-// }
+#[derive(Serialize, Debug)]
+enum EntityType {
+    CPU_AMOUNT,
+    IP_V4,
+    TIME_NOW,
+    DATE_NOW,
+}
+
+#[allow(dead_code)]
+#[derive(Serialize, Debug)]
+enum IpAddrParity {
+    EVEN,
+    ODD,
+}
+#[derive(Serialize, Debug)]
+struct CpuAmount {
+    #[serde(rename(serialize = "type"))]
+    entity_type: EntityType,
+    payload: usize,
+}
 
 #[handler]
-pub fn cpu_info() -> &'static str {
-    "Got CPU info"
+pub fn cpu_info() -> Json<CpuAmount> {
+    let mut sys = System::new();
+    sys.refresh_cpu_list(CpuRefreshKind::everything());
+
+    Json(CpuAmount {
+        entity_type: EntityType::CPU_AMOUNT,
+        payload: sys.cpus().len(),
+    })
 }
 
 #[handler]
